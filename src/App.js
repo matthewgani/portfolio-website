bdbf9f45975e9d98a3cbccef81c9093b12e42082
matthewgani <matthewgani2@gmail.com>
2022-07-08 17:00:08 +0800

    did part of tutorial for regular website

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 import Blogs from "./components/Blogs"
 import Projects from "./components/Projects"
 import Home from "./components/Home"
+import NavBar from "./components/NavBar"
 
 const App = () => {
 
@@ -22,25 +23,32 @@ const App = () => {
 
   return (
     <div>
-      <div className="Nav-bar">
-        <Link style={padding} to="/">home</Link>
-        <Link style={padding} to="/projects">projects</Link>
-        <Link style={padding} to="/blogs">blogs</Link>
-        <a style={padding}href="https://github.com/matthewgani" target="_blank" rel="noreferrer">GitHub</a>
-        <a style={padding}href="https://www.linkedin.com/in/matthewgani/" target="_blank" rel="noreferrer">LinkedIn</a>
-      </div>
-
-      <Routes>
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-
-      <div className="Footer" style={footerPadding}>
-        <i>Matthew Gani's Portfolio Website 2022</i>
-      </div>
+      <NavBar/>
     </div>
   )
+
+  // return (
+  //   <div>
+  //     <div className="Nav-bar">
+  //       <Link style={padding} to="/">home</Link>
+  //       <Link style={padding} to="/projects">projects</Link>
+  //       <Link style={padding} to="/blogs">blogs</Link>
+  //       <a style={padding}href="https://github.com/matthewgani" target="_blank" rel="noreferrer">GitHub</a>
+  //       <a style={padding}href="https://www.linkedin.com/in/matthewgani/" target="_blank" rel="noreferrer">LinkedIn</a>
+  //     </div>
+
+  //     <Routes>
+  //       <Route path="/projects" element={<Projects />} />
+  //       <Route path="/blogs" element={<Blogs />} />
+  //       <Route path="/" element={<Home />} />
+  //       {/*education section, work experience */}
+  //     </Routes>
+
+  //     <div className="Footer" style={footerPadding}>
+  //       <i>Matthew Gani's Portfolio Website 2022</i>
+  //     </div>
+  //   </div>
+  // )
 }
 
 export default App
---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+// use ai pretext 
+
+
+const NavBar = () => {
+  const [nav, setNav] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+      <ul className='hidden md:flex'>
+        <li className="p-4">Home</li>
+        <li className="p-4">Projects</li>
+        <li className="p-4">Blogs</li>
+        <li className="p-4">About</li>
+      </ul>
+      {/** only display hamburger when it hits the 768 breakpoint */}
+      <div onClick={handleNav} className='block md:hidden'>
+        {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+      </div>
+      {/**mobile menu */}
+      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+      <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
+        <ul className='uppercase p-4'>
+          <li className="p-4 border-b border-gray-600">Home</li>
+          <li className="p-4 border-b border-gray-600">Projects</li>
+          <li className="p-4 border-b border-gray-600">Blogs</li>
+          <li className="p-4">About</li>
+        </ul>
+      </div>
+
+    </div>
+  )
+}
+
+export default NavBar
